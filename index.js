fe4e709e5c591d04a919083d7d16734b02dcd879
satyam430133 <152275532+satyam430133@users.noreply.github.com>
2024-01-07 00:20:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,298 @@
+
+function change1(){
+    document.getElementById('homebutt').style.color = '#ff4904';
+    document.getElementById('homebutt').style.borderBottom = 'solid 3px #ff4904'
+    document.getElementById('homebutt').style.transform 
+}function reverse1(){
+    document.getElementById('homebutt').style.color = 'white';
+    document.getElementById('homebutt').style.borderBottom = 'none'
+}function change2(){
+    document.getElementById('aboutbutt').style.color = '#ff4904';
+    document.getElementById('aboutbutt').style.borderBottom = 'solid 3px #ff4904'
+}function reverse2(){
+    document.getElementById('aboutbutt').style.color = 'white';
+    document.getElementById('aboutbutt').style.borderBottom = 'none'
+}function change3(){
+    document.getElementById('servicebutt').style.color = '#ff4904';
+    document.getElementById('servicebutt').style.borderBottom = 'solid 3px #ff4904'
+}function reverse3(){
+    document.getElementById('servicebutt').style.color = 'white';
+    document.getElementById('servicebutt').style.borderBottom = 'none'
+}function change4(){
+    document.getElementById('portfoliobutt').style.color = '#ff4904';
+    document.getElementById('portfoliobutt').style.borderBottom = 'solid 3px #ff4904'
+}function reverse4(){
+    document.getElementById('portfoliobutt').style.color = 'white';
+    document.getElementById('portfoliobutt').style.borderBottom = 'none'
+}function change5(){
+    document.getElementById('blogbutt').style.color = '#ff4904';
+    document.getElementById('blogbutt').style.borderBottom = 'solid 3px #ff4904'
+}function reverse5(){
+    document.getElementById('blogbutt').style.color = 'white';
+    document.getElementById('blogbutt').style.borderBottom = 'none'
+}function change6(){
+    document.getElementById('contactbutt').style.color = '#ff4904';
+    document.getElementById('contactbutt').style.borderBottom = 'solid 3px #ff4904'
+}function reverse6(){
+    document.getElementById('contactbutt').style.color = 'white';
+    document.getElementById('contactbutt').style.borderBottom = 'none'
+}
+
+function changebuttoncolor(){
+    document.getElementById('aboutmebutt').style.transition = '1s'
+    document.getElementById('aboutmebutt').style.transform = 'scale(1.2)'
+    document.getElementById('aboutmebutt').style.color = 'white'
+    document.getElementById('aboutmebutt').style.backgroundColor = 'transparent'
+
+}
+function changecolorbutton(){
+    document.getElementById('aboutmebutt').style.transition = '1s'
+    document.getElementById('aboutmebutt').style.transform = 'scale(1.0)'
+    document.getElementById('aboutmebutt').style.color = '#ffffff'
+    document.getElementById('aboutmebutt').style.backgroundColor = '#ff4904'
+}
+
+function hirebuttt(){
+    document.getElementById('hirebuttt').style.transition = '1s'
+    document.getElementById('hirebuttt').style.transform = 'scale(1.2)'
+    document.getElementById('hirebuttt').style.color = 'white'
+    document.getElementById('hirebuttt').style.backgroundColor = 'transparent'
+}
+function hirebuttch(){
+    document.getElementById('hirebuttt').style.transition = '1s'
+    document.getElementById('hirebuttt').style.transform = 'scale(1.0)'
+    document.getElementById('hirebuttt').style.color = '#ffffff'
+    document.getElementById('hirebuttt').style.backgroundColor = '#ff4904'
+}
+
+function scrow1(){
+    document.getElementById("check").checked = false;
+    window.scrollTo(0,0)
+}
+function scrow2(){
+    document.getElementById("check").checked = false;
+    window.scrollTo(0,690)
+}
+function scrow3(){
+    document.getElementById("check").checked = false;
+    window.scrollTo(0,2650)
+}
+function scrow4(){
+    document.getElementById("check").checked = false;
+    window.scrollTo(0,4490)
+}
+function scrow5(){
+    document.getElementById("check").checked = false;
+    window.scrollTo(0,5690)
+}
+function scrow6(){
+    document.getElementById("check").checked = false;
+    window.scrollTo(0,6630)
+}
+
+function knwchange1(){
+    document.getElementById('knwch1').style.backgroundColor = '#ff4904'
+    document.getElementById('wd').style.color = '#ffffff'
+    document.getElementById('knwch1').style.color = '#ffffff'
+}
+function knowrecover1(){
+    document.getElementById('knwch1').style.backgroundColor = ''
+    document.getElementById('wd').style.color = '#ff4904'
+    document.getElementById('knwch1').style.color = ''
+}
+function knwchange2(){
+    document.getElementById('knwch2').style.backgroundColor = '#ff4904'
+    document.getElementById('fed').style.color = '#ffffff'
+    document.getElementById('knwch2').style.color = '#ffffff'
+}
+function knowrecover2(){
+    document.getElementById('knwch2').style.backgroundColor = ''
+    document.getElementById('fed').style.color = '#ff4904'
+    document.getElementById('knwch2').style.color = ''
+}
+function knwchange3(){
+    document.getElementById('knwch3').style.backgroundColor = '#ff4904'
+    document.getElementById('bed').style.color = '#ffffff'
+    document.getElementById('knwch3').style.color = '#ffffff'
+}
+function knowrecover3(){
+    document.getElementById('knwch3').style.backgroundColor = ''
+    document.getElementById('bed').style.color = '#ff4904'
+    document.getElementById('knwch3').style.color = ''
+}
+function knwchange4(){
+    document.getElementById('knwch4').style.backgroundColor = '#ff4904'
+    document.getElementById('rwd').style.color = '#ffffff'
+    document.getElementById('knwch4').style.color = '#ffffff'
+}
+function knowrecover4(){
+    document.getElementById('knwch4').style.backgroundColor = ''
+    document.getElementById('rwd').style.color = '#ff4904'
+    document.getElementById('knwch4').style.color = ''
+}
+function knwchange5(){
+    document.getElementById('knwch5').style.backgroundColor = '#ff4904'
+    document.getElementById('ecw').style.color = '#ffffff'
+    document.getElementById('knwch5').style.color = '#ffffff'
+}
+function knowrecover5(){
+    document.getElementById('knwch5').style.backgroundColor = ''
+    document.getElementById('ecw').style.color = '#ff4904'
+    document.getElementById('knwch5').style.color = ''
+}
+function knwchange6(){
+    document.getElementById('knwch6').style.backgroundColor = '#ff4904'
+    document.getElementById('cw').style.color = '#ffffff'
+    document.getElementById('knwch6').style.color = '#ffffff'
+}
+function knowrecover6(){
+    document.getElementById('knwch6').style.backgroundColor = ''
+    document.getElementById('cw').style.color = '#ff4904'
+    document.getElementById('knwch6').style.color = ''
+}
+
+function educh1(){
+document.getElementById('educhild1').style.backgroundColor = '#ff4904'
+document.getElementById('educhild1').style.color = '#ffffff'
+}
+function educhh1(){
+document.getElementById('educhild1').style.backgroundColor = ''
+document.getElementById('educhild1').style.color = ''
+}
+function educh2(){
+    document.getElementById('educhild2').style.backgroundColor = '#ff4904'
+    document.getElementById('educhild2').style.color = '#ffffff'
+}
+    function educhh2(){
+    document.getElementById('educhild2').style.backgroundColor = ''
+    document.getElementById('educhild2').style.color = ''
+}
+function educh3(){
+    document.getElementById('educhild3').style.backgroundColor = '#ff4904'
+    document.getElementById('educhild3').style.color = '#ffffff'
+}
+    function educhh3(){
+    document.getElementById('educhild3').style.backgroundColor = ''
+    document.getElementById('educhild3').style.color = ''
+}
+
+let store = '';
+       let val = 'Web Devloper...';
+       let arr = 0;
+        setInterval(load,200)
+        function load(){
+            if (arr < val.length){
+                store = store + val[arr];
+                document.getElementById('show').innerHTML = store;                
+                arr++;
+            }
+            else{
+                store = '';
+                arr = 0;
+            }
+        }
+
+        function submit(){
+    
+            let naam = document.getElementById('naam').value;
+            let mob = document.getElementById('mob').value;
+            let email = document.getElementById('email').value;
+            let msg = document.getElementById('msg').value;
+        
+            let object ={
+                Names:naam,
+                Number:mob,
+                Email:email,
+                Message:msg
+            }
+            if (naam === "")
+            {
+                alert (" Please Enter Your Name. ");
+                document.getElementById('naam').focus();
+            }
+            else if (mob === "")
+            {
+                alert (" Please Enter Your Mobile Number. ");
+                document.getElementById('mob').focus();
+            }
+            else if(email === "")
+            {
+                alert(" Please Enter Your Email Address. ")
+                document.getElementById('email').focus();
+            }
+            else if (!(uname.includes ('@gmail.com') || (uname.includes('@yahoo.com') || (uname.includes('@outlook.com')))))
+            {
+                alert (" Please Enter Correct Email Address.");
+            }
+            // else{
+            //     fetch('http://localhost:3000/database',{
+            //         method:"POST",
+            //         headers:{
+            //             "Content-Type":"application/json"
+            //         },
+            //         body:JSON.stringify(object)
+            //     }).then(alert("Registration Successfull"))   
+            // }
+            fetch('http://localhost:3000/database',{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(object)
+            }).then(alert("Registration Successfull"))   
+}  
+
+let counter1 = 0 
+setInterval(() => {
+    if (counter1 < 45){
+    counter1++
+    document.getElementById('count1').innerHTML = counter1
+    }
+    else if( counter2 === 45){
+        return false;
+    }
+},30);
+let counter2 = 0 
+setInterval(() => {
+    if (counter2 < 13){
+    counter2++
+    document.getElementById('count2').innerHTML = counter2
+    }
+    else if( counter2 === 13){
+        return false;
+    }
+},100);
+let counter3 = 0 
+setInterval(() => {
+    if (counter3 < 89){
+    counter3++
+    document.getElementById('count3').innerHTML = counter3
+    }
+    else if( counter3 === 89){
+        return false;
+    }
+},15);
+let counter4 = 0 
+setInterval(() => {
+    if (counter4 < 255){
+    counter4++
+    document.getElementById('count4').innerHTML = counter4
+    }
+    else if( counter4 === 255){
+        return false;
+    }
+},1);
+
+let review = 0; 
+setInterval(() => {
+    if (review === 0){
+        document.getElementById('rvimg1').style.marginLeft = '-1400px'
+        document.getElementById('rvimg1').style.transition = 'all 2s'
+        review = 1;
+    }
+    else if( review === 1){
+        document.getElementById('rvimg1').style.marginLeft = '0'
+        document.getElementById('rvimg1').style.transition = 'all 2s'
+        review = 0;
+    }
+},5000);
